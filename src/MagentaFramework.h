e701fcd9fe5089600565e35c1c78174d1685c9fe
FlightBlaze <flightblaze@gmail.com>
2019-01-12 06:38:04 +0300

    Create project

---
+++ src/MagentaFramework.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2018 Dmitry Kozichev. All rights reserved.
+ *
+ * MagentaFramework.h
+ * Created on 12 џэт. 2019 у.
+ */
+
+#pragma once
+
+
+
+
---
+++ src/MagentaFrameworkTest.cpp
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2018 Dmitry Kozichev. All rights reserved.
+ *
+ * MagentaFrameworkTest.cpp
+ * Created on 12 џэт. 2019 у.
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World!" << endl;
+	return 0;
+}
